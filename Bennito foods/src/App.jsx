9a4f8c7610707eb5d85af6f94d05617059830d1e
niobed <niiobed13@gmail.com>
2025-12-14 05:34:42 +0100

    changes

--- Bennito foods/src/App.jsx
+++ Bennito foods/src/App.jsx
@@ -1,9 +1,9 @@
-import Navbar from "./components/navbar"
-import Hero from "./components/Hero"
-import About from "./components/About"
-import Services from "./components/Services"
-import Contact from "./components/Contact"
-import Footer from "./components/Footer"
+import Navbar from "./components/Navbar.jsx"
+import Hero from "./components/Hero.jsx"
+import About from "./components/About.jsx"
+import Services from "./components/Services.jsx"
+import Contact from "./components/Contact.jsx"
+import Footer from "./components/Footer.jsx"
 
 function App() {
 
